ddd212ffcffb0fdf4cf8fbbdfa383dc69d06c92a
Stud. Roberta Tamme <r.tamme.0690@vallauri.net>
2020-10-09 13:01:36 +0200

    09/10/20

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-main',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   names = ["Barak Obama","Filippo","Luke Hemmings","Calum Hood","Michael Clifford","Ashton Irwin","Camilla Mala","Edoardo Pavese","Bloody Mery"];
   myName = 'Roberta Tammè';
+
+  
   changeName(){
     this.myName= this.names[Math.floor(Math.random()*8)];
   }
